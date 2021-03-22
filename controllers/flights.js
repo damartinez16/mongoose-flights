1cdc2dde09efbe8eb57e989b6587cadaf98158c9
Danielle Martinez <daniellealexandria10@gmail.com>
2021-03-21 21:18:13 -0600

    new flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -12,9 +12,14 @@ function index(req, res) {
    });
   }
 
-function newFlight(req, res) {
-    res.render('flights/new');
-}
+  function newFlight(req, res) {
+    const newFlight = new Flight();
+  const dt = newFlight.departs;
+  let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+        departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+  res.render('flights/new', {departsDate});
+  }
+
 
 function create(req, res) {
   const flight = new Flight(req.body);
@@ -23,8 +28,4 @@ function create(req, res) {
     console.log(flight);
     res.redirect('/flights');
   });
-}
-
-function newFlight(req, res) {
-    res.render('flights/new');
 }
--- models/flight.js
+++ models/flight.js
@@ -4,9 +4,19 @@ const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
     airline: String,
-    flightNo: Number,
-    airport: String,
-    departs: Date
+    flightNo: {
+        type: Number, 
+        min: 10,
+        max: 9999
+    },
+    airport: {
+        type: String, 
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    departs: {type: Date, default: function() {
+        return new Date(new Date().setFullYear(new Date().getFullYear()+ 1))
+    } }
 }, {
     timestamps: true
 });
